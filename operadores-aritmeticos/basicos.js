e13a316291f3f2bd7af012a3f83ad33fedac392e
Joana Maria <joanamariacardoso1601@gmail.com>
2025-03-15 20:40:33 +0000

    operadores basicos

---
+++ operadores-aritmeticos/basicos.js
@@ -0,0 +1,9 @@
+// Operadpres aritméticos básicos
+
+console.log(2 + 2); // 4 -> adição
+console.log(2 - 2); // 0 -> subtração
+console.log(2 * 2); // 4 -> multiplicação
+console.log(2 / 2); // 1 -> divisão
+console.log(2 % 1); // 0 -> resto da divisão
+console.log(2 ** 2); // 4 -> 2 elevado a 2
+console.log(2 ** 3); // 8 -> 2 elevado a 3
