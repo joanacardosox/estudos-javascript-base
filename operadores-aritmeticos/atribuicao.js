2cbdf486a5c41aaaaf108e09724c5cb6992014e1
Joana Maria <joanamariacardoso1601@gmail.com>
2025-03-15 20:54:05 +0000

    operadores aritmeticos

---
+++ operadores-aritmeticos/atribuicao.js
@@ -0,0 +1,22 @@
+//Operadores de atribuição -> atribuem valores a variáveis
+
+let x = 10;
+
+x += 5; // x = x + 5
+console.log(x); // 15
+
+x -= 5; // x = x - 5
+console.log(x); // 10
+
+x *= 5; // x = x * 5
+console.log(x); // 50
+
+x /= 5; // x = x / 5
+console.log(x); // 10
+
+x %= 5; // x = x % 5
+console.log(x); // 0
+
+x **= 5; // x = x ** 5
+console.log(x); // 0
+
---
+++ operadores-aritmeticos/incremento-decremento.js
@@ -0,0 +1,9 @@
+//OPERADORES DE INCREMENTO E DECREMENTO -> ++ e --
+
+let x = 7;
+
+console.log(x++); // 7
+console.log(x); // 8
+
+console.log(--x); // 7
+console.log(x); // 7
