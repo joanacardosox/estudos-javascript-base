6ae367ddcd39658616510ab7eb4e6412f95df45b
Joana Maria <joanamariacardoso1601@gmail.com>
2025-03-15 20:37:09 +0000

    operadores:not e or

---
+++ operadores/not.js
@@ -0,0 +1,6 @@
+// NOT (!) - Operador de negação
+
+console.log(!true); // false
+console.log(!false); // true
+console.log(!0); // false
+console.log(!"Joana");
---
+++ operadores/or.js
@@ -0,0 +1,9 @@
+// OR ( || ) - Retorna TRUE se um dos operandos for TRUE
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log(0 || "Joana"); // Joana -> ele retorna o primeiro valor verdadeiro
+console.log("Joana" || 0); // Joana -> ele retorna o primeiro valor verdadeiro
